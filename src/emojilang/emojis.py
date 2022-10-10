526827e4c6ec62a3b02816721d7a2f1fd981b4a5
Castor Mann <Castormannen@gmail.com>
2022-10-10 09:12:13 +0200

    added some emojis + grammatics

---
+++ src/emojilang/emojis.py
@@ -0,0 +1,59 @@
+# DIGITS IN BASE 12
+ZERO = "🕛"                     # THE DIGIT ZERO (0)
+ONE = "🕐"                      # THE DIGIT ONE (1)
+TWO = "🕑"                      # THE DIGIT TWO (2)
+THREE = "🕒"                    # THE DIGIT THREE (3)          
+FOUR = "🕓"                     # THE DIGIT FOUR (4)
+FIVE = "🕔"                     # THE DIGIT FIVE (5)
+SIX = "🕕"                      # THE DIGIT SIX (6)
+SEVEN = "🕖"                    # THE DIGIT SEVEN (7)
+EIGHT = "🕗"                    # THE DIGIT EIGHT (8)
+NINE = "🕘"                     # THE DIGIT NINE (9)
+TEN = "🕙"                      # THE DIGIT "TEN" (A) base 12
+ELEVEN = "🕚"                   # THE DIGIT "ELEVEN" (B) base 12
+
+
+#ALPHABET
+ANGRY = "😡"                    
+HOT = "🥵"
+COLD = "🥶"
+SCARED = "😨"
+TIRED = "🥱"
+HAPPY_DEVIL = "😈"
+ANGRY_DEVIL = "👿"
+SICK = "🤢"
+CLOWN = "🤡"
+SHIT = "💩"
+SKULL = "💀"
+
+
+# ARITHMETIC
+HAPPY = "😀"                    # ADDITION (+)
+LAUGHING = "😂"                 # MULTIPLICATION (*)
+SAD = "☹️"                      # SUBTRACTION (-)
+CRYING = "😭"                   # (INTEGER)DIVISION (/)
+CLAP = "👏"                     # VARIABLE ASSIGNMENT (=)
+
+
+# COMPARISONS
+FINGER_UP = "👆"                # GREATER THAN (>)
+FINGER_DOWN = "👇"              # LESS THAN (<)
+THUMB_UP = "👍"                 # EQUALS (==)
+THUMB_DOWN = "👎"               # NOT EQUAL (!=)
+
+
+# BOOLEAN LOGIC
+TWO_FINGERS = "✌️"              # LOGICAL OR (||)
+CROSSED_FINGERS = "🤞"          # LOGICAL AND (&&)
+RED_X = "❌"                    # LOGICAL NOT (!)
+
+
+# FLOW CONTROL
+FINGER_RIGHT = "👉"             # IF ([CONDITION] 👉 [BLOCK OF CODE])
+FINGER_LEFT = "👈"              # ELSE ([CONDITION] 👉 [BLOCK OF CODE] [BLOCK OF CODE] 👈)
+FINGER_OK = "👌"                # WHILE ([CONDITION] 👌 [BLOCK OF CODE])
+
+
+# IO
+EYES = "👀"                     # OUTPUT (print())
+
