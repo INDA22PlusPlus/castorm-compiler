b4771f660f6c2081c479eac512ac1883ed947ade
Castor Mann <Castormannen@gmail.com>
2022-11-03 16:04:51 +0100

    interpreter to python

---
+++ src/emojilang/compiler.py
@@ -0,0 +1,231 @@
+from emojis import *
+
+
+
+INDENT = "    "
+class Compiler:
+    def __init__(self, data) -> None:
+        self.original_data = data
+        self.data = list(filter(lambda x: x in DEFINITIONS, data))
+        self.pointer = 0
+        self.count = len(self.data)
+        self.result = ""
+        self.indent_level = 0
+
+    def throw(self, expected, found):
+        raise SyntaxError(f"Expected Symbol: {expected}. Found: {found}. At: {self.pointer}: Program:\n {self.result}")
+
+    def pad(self):
+        for i in range(self.indent_level):
+            self.result += INDENT
+    def append(self, data):
+        
+        self.result += data
+
+    def current_symbol(self) -> str:
+        if self.EOF():
+            return None
+        return self.data[self.pointer]
+
+    def EOF(self) -> bool:
+        return self.pointer >= self.count
+
+    def compile(self) -> str:
+        while not self.EOF():
+            self.expect_statement()
+        return self.result
+
+    def expect_symbol(self, symbol) -> None:
+        curr = self.current_symbol()
+        if curr != symbol:
+            self.throw(symbol, curr)
+        self.pointer += 1
+    def expect_statement(self) -> None:
+        self.pad()
+        curr = self.current_symbol()
+        if curr in ALPHABET:
+            self.expect_assignment()
+        elif curr in FLOW:
+            self.expect_flow_control()
+        elif curr == OUTPUT:
+            self.expect_output()
+        else:
+            self.throw("statement", curr)
+    def expect_assignment(self) -> None:
+        self.expect_variable()
+        curr = self.current_symbol()
+        if curr == ASSIGNMENT:
+            self.append("=")
+            self.pointer += 1
+            self.expect_expression()
+            self.expect_terminator()
+        else:
+            self.throw(ASSIGNMENT, curr)
+    def expect_variable(self) -> None:
+        curr = self.current_symbol()
+        while curr in ALPHABET:
+            self.append(ALPHABET[curr])
+            self.pointer += 1
+            curr = self.current_symbol()
+    def expect_expression(self) -> None:
+        curr = self.current_symbol()
+        if curr == INPUT:
+            self.append("int(input())")
+            self.pointer += 1
+            return
+        self.expect_term()
+        curr = self.current_symbol()
+        if curr == ADDITION:
+            self.append("+")
+            self.pointer += 1
+            self.expect_expression()
+        elif curr == SUBTRACTION:
+            self.append("-")
+            self.pointer += 1
+            self.expect_expression()
+    def expect_term(self) -> None:
+        self.expect_factor()
+        curr = self.current_symbol()
+        if curr == MULTIPLICATION:
+            self.append("*")
+            self.pointer += 1
+            self.expect_term()
+        elif curr == DIVISION:
+            self.append("//")
+            self.pointer += 1
+            self.expect_term()
+    def expect_factor(self) -> None:
+        curr = self.current_symbol()
+        if curr in ALPHABET:
+            self.expect_variable()
+        elif curr in DIGITS:
+            self.expect_integer()
+        elif curr == LP:
+            self.append("(")
+            self.pointer += 1
+            self.expect_expression()
+            nxt = self.current_symbol()
+            if nxt == RP:
+                self.append(")")
+                self.pointer += 1
+            else:
+                self.throw(RP, nxt)
+        else:
+            self.throw("*factor*", curr)
+    def expect_integer(self) -> None:
+        curr = self.current_symbol()
+        i = ""
+        while curr in DIGITS:
+            i += curr
+            self.pointer += 1
+            curr = self.current_symbol()
+        self.append(emoint_to_pyint(i))
+    def expect_terminator(self) -> None:
+        if self.EOF():
+            return
+        self.expect_symbol(NEWLINE)
+        self.append("\n")
+    def expect_output(self) -> None:
+        self.expect_symbol(OUTPUT)
+        self.append("print")
+        self.expect_symbol(LP)
+        self.append("(")
+        self.expect_expression()
+        self.expect_symbol(RP)
+        self.append(")")
+        self.expect_terminator()
+    def expect_flow_control(self) -> None:
+        curr = self.current_symbol()
+        if curr == LOOP:
+            self.expect_loop()
+        elif curr == IF:
+            self.expect_if()
+        else:
+            self.throw("if/while", curr)
+    def expect_loop(self) -> None:
+        self.expect_symbol(LOOP)
+        self.append("while ")
+        self.expect_symbol(LP)
+        self.expect_condition()
+        self.expect_symbol(RP)
+        self.append(":\n")
+        self.indent_level += 1
+        self.expect_block()
+        self.indent_level -= 1
+    def expect_block(self) -> None:
+        self.expect_symbol(LB)
+        curr = self.current_symbol()
+        while curr != RB:
+            self.expect_statement()
+            curr = self.current_symbol()
+        self.expect_symbol(RB)
+    def expect_if(self) -> None:
+        self.expect_symbol(IF)
+        self.append("if ")
+        self.expect_symbol(LP)
+        self.expect_condition()
+        self.expect_symbol(RP)
+        self.append(":\n")
+        self.indent_level += 1
+        self.expect_block()
+        self.indent_level -= 1
+        curr = self.current_symbol()
+        if curr == ELSE:
+            self.expect_symbol(ELSE)
+            self.append("else:")
+            self.indent_level += 1
+            self.expect_block()
+            self.indent_level -= 1
+    def expect_condition(self) -> None:
+        self.expect_term_bool()
+        curr = self.current_symbol()
+        if curr == OR:
+            self.append(" or ")
+            self.pointer += 1
+            self.expect_condition()
+    def expect_term_bool(self) -> None:
+        self.expect_factor_bool()
+        curr = self.current_symbol()
+        if curr == AND:
+            self.append(" and ")
+            self.pointer += 1
+            self.expect_factor_bool()
+    def expect_factor_bool(self) -> None:
+        curr = self.current_symbol()
+        if curr in BOOLS:
+            self.append(BOOLS[curr])
+            self.pointer += 1
+        else:
+            self.expect_comparison()
+    def expect_comparison(self) -> None:
+        self.expect_expression()
+        curr = self.current_symbol()
+        if curr in COMPARISONS:
+            self.append(COMPARISONS[curr])
+            self.pointer += 1
+        else:
+            self.throw("comparison", curr)
+        self.expect_expression()
+
+    def run(self) -> None:
+        exec(self.compile())
+
+def main():
+    x = ""
+    data = f"{A}{B}{ASSIGNMENT}{SEVEN}{SUBTRACTION}{FIVE}{NEWLINE}{OUTPUT}{LP}{A}{B}{RP}{NEWLINE}{LOOP}{LP}{A}{B}{LT}{SEVEN}{SEVEN}{RP}{LB}{A}{B}{ASSIGNMENT}{A}{B}{ADDITION}{ONE}{NEWLINE}{OUTPUT}{LP}{A}{B}{RP}{NEWLINE}{RB}"
+
+    
+    compiler = Compiler(data)
+    print(data)
+    program = compiler.compile()
+    print(program)
+    exec(program)
+
+import os
+import sys
+import codecs
+
+with codecs.open(os.path.join(sys.path[0], sys.argv[1]), encoding="utf-8") as f:
+    compiler = Compiler("".join(f.readlines()))
+    compiler.run()
+    
--- src/emojilang/emojis.py
+++ src/emojilang/emojis.py
@@ -11,49 +11,93 @@ EIGHT = "🕗"                    # THE DIGIT EIGHT (8)
 NINE = "🕘"                     # THE DIGIT NINE (9)
 TEN = "🕙"                      # THE DIGIT "TEN" (A) base 12
 ELEVEN = "🕚"                   # THE DIGIT "ELEVEN" (B) base 12
+DIGITS = { ZERO: 0, ONE: 1, TWO: 2, THREE: 3, FOUR: 4, FIVE: 5, SIX: 6, SEVEN: 7, EIGHT: 8, NINE: 9, TEN: 10, ELEVEN: 11 }
+DIGITS_REVERSED = { 0: ZERO, 1: ONE, 2: TWO, 3: THREE, 4: FOUR, 5: FIVE, 6: SIX, 7: SEVEN, 8: EIGHT, 9: NINE, 10: TEN, 11: ELEVEN }
+def emoint_to_pyint(string: str) -> str:
+    idx = 0
+    tot = 0
+    for c in string[::-1]:
+        if c not in DIGITS.keys(): 
+            return None
+        tot += DIGITS[c] * 12**idx
+        idx += 1
+    return str(tot)
+
+def pyint_to_emoint(string: str) -> str:
+    try:
+        i = int(string)
+        idx = 0
+        res = ""
+        while i != 0:
+            res = DIGITS_REVERSED[i % 12] + res
+            i //= 12
+        return res
+    except:
+        return None
 
 
 #ALPHABET
-ANGRY = "😡"                    
-HOT = "🥵"
-COLD = "🥶"
-SCARED = "😨"
-TIRED = "🥱"
-HAPPY_DEVIL = "😈"
-ANGRY_DEVIL = "👿"
-SICK = "🤢"
-CLOWN = "🤡"
-SHIT = "💩"
-SKULL = "💀"
+A = "😡"                    
+B = "🥵"
+C = "🥶"
+D = "😨"
+E = "🥱"
+F = "😈"
+G = "👿"
+H = "🤢"
+I = "🤡"
+J = "💩"
+K = "💀"
+ALPHABET = { A: "A", B: "B", C: "C", D: "D", E: "E", F: "F", G: "G", H: "H", I: "I", J: "J", K: "K" }
+#ALPHABET_REVERSED = { "A": A, "B": B, "C": C, "D": D, "E": E, "F": F, "G": G, "H": H, "I": I, "J": J, "K": K }
 
 
 # ARITHMETIC
-HAPPY = "😀"                    # ADDITION (+)
-LAUGHING = "😂"                 # MULTIPLICATION (*)
-SAD = "☹️"                      # SUBTRACTION (-)
-CRYING = "😭"                   # (INTEGER)DIVISION (/)
-CLAP = "👏"                     # VARIABLE ASSIGNMENT (=)
+ADDITION = "🙂"                         # ADDITION (+)
+MULTIPLICATION = "😂"                   # MULTIPLICATION (*)
+SUBTRACTION = "🙃"                      # SUBTRACTION (-)
+DIVISION = "😭"                         # (INTEGER)DIVISION (/)
+ARITHMETIC = { ADDITION: "+", SUBTRACTION: "-", MULTIPLICATION: "*", DIVISION: "//"}
+
 
 
 # COMPARISONS
-FINGER_UP = "👆"                # GREATER THAN (>)
-FINGER_DOWN = "👇"              # LESS THAN (<)
-THUMB_UP = "👍"                 # EQUALS (==)
-THUMB_DOWN = "👎"               # NOT EQUAL (!=)
-
+GT = "👆"                       # GREATER THAN (>)
+LT = "👇"                       # LESS THAN (<)
+EQ = "👍"                       # EQUALS (==)
+NE = "👎"                       # NOT EQUAL (!=)
+COMPARISONS = { GT: ">", LT: "<", EQ: "==", NE: "!=" }
 
 # BOOLEAN LOGIC
-TWO_FINGERS = "✌️"              # LOGICAL OR (||)
-CROSSED_FINGERS = "🤞"          # LOGICAL AND (&&)
-RED_X = "❌"                    # LOGICAL NOT (!)
-
+OR = "✌️"                       # LOGICAL OR (||)
+AND = "🤞"                      # LOGICAL AND (&&)
+NOT = "❌"                      # LOGICAL NOT (!)
+TRUE = "✅"
+FALSE = "❎"
+LOGIC = { OR: "or", AND: "and", NOT: "not" }
+BOOLS = { TRUE: "True", FALSE: "False"}
 
 # FLOW CONTROL
-FINGER_RIGHT = "👉"             # IF ([CONDITION] 👉 [BLOCK OF CODE])
-FINGER_LEFT = "👈"              # ELSE ([CONDITION] 👉 [BLOCK OF CODE] [BLOCK OF CODE] 👈)
-FINGER_OK = "👌"                # WHILE ([CONDITION] 👌 [BLOCK OF CODE])
-
+IF = "👉"                   # IF ([CONDITION] 👉 [BLOCK OF CODE])
+ELSE = "👈"                 # ELSE ([CONDITION] 👉 [BLOCK OF CODE] 👈 [BLOCK OF CODE])
+LOOP = "👌"                 # WHILE (👌 [BLOCK OF CODE])
+FLOW = { IF: "if", ELSE: "else", LOOP: "while True:"}
 
 # IO
-EYES = "👀"                     # OUTPUT (print())
+OUTPUT = "👄"                     # OUTPUT (print())
+INPUT = "👂"
+IO = {OUTPUT: "print", INPUT: "input()"}
+
+
+# OTHER
+NEWLINE = "🚩"
+ASSIGNMENT = "👏"                       # VARIABLE ASSIGNMENT (=)
+
+LP = "🤛"
+RP = "🤜"
+
+LB = "👐"
+RB = "🙌"
+
+DEFINITIONS = set([NEWLINE, ASSIGNMENT, LP, RP, LB, RB] + list(IO.keys()) + list(FLOW.keys()) + list(LOGIC.keys()) + list(COMPARISONS.keys()) + list(ARITHMETIC.keys()) + list(ALPHABET.keys()) + list(DIGITS.keys()))
 
